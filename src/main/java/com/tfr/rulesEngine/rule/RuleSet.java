a3da0099fb64a9981d51034c9a98424e28b4f4f4
Anthony Garo <agaro1121@gmail.com>
2017-06-26 21:11:41 -0400

    random crap

--- src/main/java/com/tfr/rulesEngine/rule/RuleSet.java
+++ src/main/java/com/tfr/rulesEngine/rule/RuleSet.java
@@ -3,55 +3,39 @@ package com.tfr.rulesEngine.rule;
 import com.google.common.collect.Sets;
 import com.tfr.rulesEngine.exception.DuplicateRuleException;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  *
  * Created by Erik on 6/14/2017.
  */
 //TODO R extends Rule<I,O>
-public abstract class RuleSet<I,O,R extends Rule<I,O>> implements Iterable<R> {
+public abstract class RuleSet<I,O,R extends Rule<I,O>> extends TreeSet<R> {
 
     private String name;
-    private Set<R> rules;
 
     public RuleSet(String name) {
-        this(name, Sets.newTreeSet());
+        this.name = name;
     }
 
     public RuleSet(String name, Set<R> rules) {
         this.name = name;
-        this.rules = Sets.newTreeSet();
-        rules.forEach(this::add);
+        addAll(rules);
     }
 
     public String getName() {
         return this.name;
     }
 
-    public Set<R> getRules() {
-        return this.rules;
-    }
-
+    @Override
     public boolean add(R rule) {
-        boolean addedSuccessfully = rules.add(rule);
+        boolean addedSuccessfully = super.add(rule);
         if(!addedSuccessfully) {
             throw new DuplicateRuleException(rule);
         }
         return addedSuccessfully;
     }
-
-    public boolean contains(R rule) {
-        return rules.contains(rule);
-    }
-
-    public boolean remove(R rule) {
-        return rules.remove(rule);
-    }
-
-    public Iterator<R> iterator() {
-        return rules.iterator();
-    }
-
 }
